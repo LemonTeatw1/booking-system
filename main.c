fec0527ac7beae73cf8b3babf5304d85e02e74cd
Alvin <laol04916@gmail.com>
2024-06-05 21:29:33 +0800

    Update

--- main.c
+++ main.c
@@ -6,8 +6,9 @@ int main(void) {
   int a, b, c;
   int l = 0;
 
-  char dailytime[13] = {'9', '10', '11', '12', '13', '14', '15',
-                       '16', '17', '18', '19', '20', '21'};
+  // 字符串陣列來存儲時間值
+  const char* dailytime[13] = {"9", "10", "11", "12", "13", "14", "15",
+                               "16", "17", "18", "19", "20", "21"};
 
   char tell[1000], n[1000], date[1000], time[1000];
   char name[1000][1000], phone[1000][1000], datetime[1000][1000],
@@ -45,7 +46,7 @@ int main(void) {
 
         if (found) {
           printf("已有定位\n");
-          printf("您的資料如下");
+          printf("您的資料如下\n");
           printf("大名:%s\n", n);
           printf("電話號碼:%s\n", tell);
           printf("定位日期:%s\n", date);
@@ -53,88 +54,146 @@ int main(void) {
         } else {
           strcpy(phone[l], tell);
           strcpy(name[l], n);
-          l = l + 1;
+          l++;
           printf("請輸入日期:");
           scanf("%s", date);
           printf("請輸入時間:");
           scanf("%s", time);
 
           int found2 = 0;
-          for (int j = 0; j < 20; j++) {
-            if (strcmp(dailytime, datetime[j]) == 0 && strcmp(n, name[j]) == 0) {
+          for (int j = 0; j < 1000; j++) {
+            if (strcmp(time, clock[j]) == 0 && strcmp(date, datetime[j]) == 0) {
               found2 = 1;
               break;
             }
-            if (found2) {
-              printf("已滿位\n");
-              printf("以下是合理的時間\n");
-              for (found2 = 1; found2 < 20; found2++) {
-                printf("時間:%s\n", time);
-              }
-            } else {
-              for (int k = 0; k < 1000; k++) {
-                strcpy(datetime[k], date);
-                strcpy(clock[k], time);
-              }
+          }
+
+          if (found2) {
+            printf("已滿位\n");
+            printf("以下是合理的時間\n");
+            for (int k = 0; k < 13; k++) {
+              printf("時間:%s\n", dailytime[k]);
             }
+          } else {
+            strcpy(datetime[l-1], date);
+            strcpy(clock[l-1], time);
+            printf("已新增定位\n");
           }
         }
         break;
       }
+
       case 2: {
         printf("請輸入名字:");
-        scanf("%s",n);
+        scanf("%s", n);
         printf("請輸入電話號碼:");
-        scanf("%s",tell);
+        scanf("%s", tell);
 
         int found3 = 0;
-        for (int p = 0; p < 1000; p++) {
-          if (strcmp(phone[p], tell) == 0 && strcmp(name[p], n) == 0) {
+        int p = -1; // 用於保存匹配的索引
+        for (int i = 0; i < 1000; i++) {
+          if (strcmp(phone[i], tell) == 0 && strcmp(name[i], n) == 0) {
             found3 = 1;
+            p = i;
             break;
           }
-          if(found3){
-            printf("請問你要修改日期嗎?\n");
-            scanf("%d",&b);
-            if(b == 1){
-              printf("請輸入新日期:");
-              scanf("%s",date);
-            }
-            else if(b == 0){
-              printf("不用輸入");
-              }
-            
-              printf("你要修改的時間為何?");
-              printf("請輸入新時間:");
-              scanf("%s",time);
-              int found4 = 0;
-              for (int m = 0; m < 20; m++) {
-                if (strcmp(dailytime, datetime[m]) == 0 && strcmp(n, name[m]) == 0) 
-                  found4 = 1;
-                  break;
-                }
-                if (found4) {
-                  printf("已滿位\n");
-                  printf("以下是合理的時間\n");
-                  for (found4 = 1; found4 < 20; found4++) {
-                    printf("時間:%s\n", time);
-                  }
-                } else {
-                  for (int k = 0; k < 1000; k++) {
-                    strcpy(datetime[k], date);
-                    strcpy(clock[k], time);
-                  }
-                }
-                strcpy(clock[p], time);
-                printf("已修改\n");
-              
+        }
+
+        if (found3) {
+          printf("請問你要修改日期嗎? (1是/0否)\n");
+          scanf("%d", &b);
+          if (b == 1) {
+            printf("請輸入新日期:");
+            scanf("%s", date);
+          }
+
+          printf("請輸入新時間:");
+          scanf("%s", time);
+
+          int found4 = 0;
+          for (int m = 0; m < 1000; m++) {
+            if (strcmp(time, clock[m]) == 0 && strcmp(date, datetime[m]) == 0) {
+              found4 = 1;
+              break;
             }
           }
 
-        case 3:{
+          if (found4) {
+            printf("已滿位\n");
+            printf("以下是合理的時間\n");
+            for (int k = 0; k < 13; k++) {
+              printf("時間:%s\n", dailytime[k]);
+            }
+          } else {
+            if (b == 1) {
+              strcpy(datetime[p], date); // 修改日期
+            }
+            strcpy(clock[p], time); // 修改時間
+            printf("已修改\n");
+          }
+        } else {
+          printf("未找到匹配的記錄\n");
         }
+        break;
+      }
+
+      case 3: {
+        printf("請輸入名字:");
+        scanf("%s", n);
+        printf("請輸入電話號碼:");
+        scanf("%s", tell);
+
+        int found5 = 0;
+        for (int q = 0; q < 1000; q++) {
+          if (strcmp(phone[q], tell) == 0 && strcmp(name[q], n) == 0) {
+            found5 = 1;
+            for (int r = q; r < 999; r++) {
+              strcpy(name[r], name[r + 1]);
+              strcpy(phone[r], phone[r + 1]);
+              strcpy(datetime[r], datetime[r + 1]);
+              strcpy(clock[r], clock[r + 1]);
+            }
+            l--;
+            break;
+          }
+        }
+
+        if (found5) {
+          printf("已取消\n");
+        } else {
+          printf("未找到匹配的記錄\n");
+        }
+        break;
+      }
+
+      case 4: {
+        printf("請輸入電話號碼:");
+        scanf("%s", tell);
+
+        int found6 = 0;
+        for (int s = 0; s < 1000; s++) {
+          if (strcmp(phone[s], tell) == 0) {
+            found6 = 1;
+            printf("大名:%s\n", name[s]);
+            printf("電話號碼:%s\n", phone[s]);
+            printf("定位日期:%s\n", datetime[s]);
+            printf("定位時間:%s點\n", clock[s]);
+            break;
+          }
+        }
+
+        if (!found6) {
+          printf("未找到匹配的記錄\n");
+        }
+        break;
+      }
+
+      default: {
+        printf("無效的選項\n");
+        break;
       }
     }
   }
-  return 0; 
- }
+
+  return 0;
+}
